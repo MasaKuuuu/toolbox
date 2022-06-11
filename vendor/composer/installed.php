c0ec8c1b4f96f97aad5f9af4b0c944bbc7b2f4a9
MasaKu <masaki_kuyama@yahoo.co.jp>
2022-06-11 13:14:11 +0900

    composer.json の更新

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,22 +1,22 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => NULL,
+        'reference' => 'e6c30baeee61bfd352ec67495f7d19ae4a46158d',
         'name' => '__root__',
         'dev' => true,
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => NULL,
+            'reference' => 'e6c30baeee61bfd352ec67495f7d19ae4a46158d',
             'dev_requirement' => false,
         ),
         'doctrine/instantiator' => array(
